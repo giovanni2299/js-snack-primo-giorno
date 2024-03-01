ab84957e02fc461f71123c5d54426e2c0e42fe66
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-01 13:36:30 +0100

    ADD: exercise 2B 2C and 3

--- js/app.js
+++ js/app.js
@@ -4,9 +4,12 @@
 // Dichiara una variabile chiamata number e assegnale il valore 100.
 //  Stampa il valore della variabile in console.
 
-let number = 100; //number
+let number = 100; //number 
 
-console.log(number); //number 
+console.log('ESERCIZIO 1:',number); //number 
+
+
+
 
 //Esercizio 2
 //Dichiara una costante chiamata PI e assegnale il valore di 3.14. 
@@ -14,10 +17,40 @@ console.log(number); //number
 
 const PI = 3.14; //number
 
-console.log(PI) //number
+console.log('ESERCIZIO 2:',PI) //number
+
+
+
 
 //Esercizio 2/B
 //Prova ad assegnare alla costante chiamata PI il valore di 3.1416. 
 //Sai dire cosa succede?
 
-//const PI = 3.1416; //number 
+//const PI = 3.1416; //number //non funziona perchè const non è una variabile riassegnabile
+
+
+
+
+//Esercizio 2/C
+//Dichiara una variabile chiamata radius e assegnale il valore 8.
+//Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+//Stampa il valore della variabile circle in console.
+
+let radius = 8; //number
+
+let circle = (radius * 2 * PI); //number
+
+console.log('ESERCIZIO 2/C:' ,circle) //number
+
+
+//Esercizio 3
+//Dichiara una variabile chiamata name e
+//assegnale il valore “Carlo”.
+//Successivamente, modifica il valore della variabile in “Marco”.
+//Stampa il nuovo valore in console.
+
+let name = 'Carlo'; //string
+
+name = 'Marco'; //string
+
+console.log('ESERCIZIO 3:' ,name); //string
