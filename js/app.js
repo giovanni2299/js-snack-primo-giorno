6f5e73ac78fd212ccbe749e199c58c9a7d01f004
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-01 12:44:27 +0100

    ADD: created file and start whit the exercise

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// console.log('ciao mondo')
+
+// ESERCIZIO 1 
+// Dichiara una variabile chiamata number e assegnale il valore 100.
+//  Stampa il valore della variabile in console.
+
+let number = 100; //number
+
+console.log(number); //number 
+
+//Esercizio 2
+//Dichiara una costante chiamata PI e assegnale il valore di 3.14. 
+//Stampa il valore della costante in console.
+
+const PI = 3.14; //number
+
+console.log(PI) //number
+
+//Esercizio 2/B
+//Prova ad assegnare alla costante chiamata PI il valore di 3.1416. 
+//Sai dire cosa succede?
+
+//const PI = 3.1416; //number 
